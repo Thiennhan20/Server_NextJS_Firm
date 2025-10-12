597115666fd2d2c813bae8617d8bc4bbe63ed4f4
Thiện Nhân <nhanntn2203@gmail.com>
2025-10-12 08:16:25 +0700

    fix file server by 3 (helmet and cors)

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/api/test-email-config', (req, res) => {
   }
   
   try {
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
